ead0c8bc8f6d52012f1aaf4b59f71024f01fc66e
Daniel <81585579+miyeso@users.noreply.github.com>
2021-03-29 21:23:35 +0300

    Add files via upload

---
+++ js/change.js
@@ -0,0 +1,130 @@
+
+function heroeschange1(){
+    document.getElementById("changeimg").src="../images/Altair.png";
+    document.getElementById("changename").innerHTML="Альтаир ибн Ла-Ахад";
+    document.getElementById("changetext").innerHTML="Альтаир ибн Ла-Ахад  — Мастер Ассассин и Наставник из сирийского Братства Ассассинов, а также предок Дезмонда Майлса.";
+}
+function heroeschange2(){
+    document.getElementById("changeimg").src="../images/Almualim.png";
+    document.getElementById("changename").innerHTML="Аль-Муалим";
+    document.getElementById("changetext").innerHTML="Рашид ад-Дин Синан, наиболее известный как Аль-Муалим ― первый упоминающийся наставник ордена ассасинов, а также учитель Альтаира ибн Ла-Ахада.";
+}
+function heroeschange3(){
+    document.getElementById("changeimg").src="../images/Ezio.png";
+    document.getElementById("changename").innerHTML="Эцио Аудиторе да Фиренце";
+    document.getElementById("changetext").innerHTML="Эцио Аудиторе да Фиренце — ассасин-наставник, бывший дворянин.";
+}
+function heroeschange4(){
+    document.getElementById("changeimg").src="../images/Machiavelli.png";
+    document.getElementById("changename").innerHTML="Никколо Макиавелли";
+    document.getElementById("changetext").innerHTML="Никколо ди Бернардо Макиавелли ― итальянский философ, ученый, писатель, политический деятель (занимал во Флоренции пост государственного секретаря). Был наставником ассассинов, затем передал пост Эцио";
+}
+function heroeschange5(){
+    document.getElementById("changeimg").src="../images/Leonardo.png";
+    document.getElementById("changename").innerHTML="Леонардо Винчи";
+    document.getElementById("changetext").innerHTML="Леонардо ди сер Пьеро да Винчи — анатом, скульптор, картограф, художник, ботаник, инженер, архитектор, математик и один из крупнейших представителей искусства Высокого Возрождения.";
+}
+function heroeschange6(){
+    document.getElementById("changeimg").src="../images/Lorenzo.png";
+    document.getElementById("changename").innerHTML="Лоренцо Медичи";
+    document.getElementById("changetext").innerHTML="Лоренцо Медичи ― итальянский государственный деятель и де-факто правитель Флорентийской Республики в эпоху итальянского Ренессанса. Являлся тайным союзником Ассассинов, особенно семьи Аудиторе";
+}
+function heroeschange7(){
+    document.getElementById("changeimg").src="../images/Rodrigo.png";
+    document.getElementById("changename").innerHTML="Родриго Борджиа";
+    document.getElementById("changetext").innerHTML="Родриго Борджиа, настоящее имя Родерик Ланзол де Борха — папа римский, магистр ордена тамплиеров и главный враг семьи Аудиторе. Когда Родриго стал римским папой, получил имя Александр VI.";
+}
+
+
+
+
+function groupchange1(){
+    document.getElementById("changeimg").src="../images/assassin's_logo.png";
+    document.getElementById("changename").innerHTML="Орден Ассассинов";
+    document.getElementById("changetext").innerHTML="Ассасины - орден, защищающий человеческое право на свободу выбора. Их идеология основана на вере, что только свобода воли поможет человечеству в своих свершениях, в рождении новых идей и инноваций. И хотя Ордену понятны все недостатки такой свободы, но они ничто по сравнению с последствиями её потери.Орден видит и принимает всю неидеальность человечества - в конце концов, только с помощью метода проб и ошибок можно прийти к идеализированию общества и сделать источник мудрости и знаний открытым для всех.";
+}
+function groupchange2(){
+    document.getElementById("changeimg").src="../images/tamplier's_logo.png";
+    document.getElementById("changename").innerHTML="Орден Тамплиеров";
+    document.getElementById("changetext").innerHTML="Орден Тамплиеров - орден с многовековой историей. И хотя орден в разное время использовала различные названия, принято считать, что она возникла еще на заре человечества. История говорит нам, что все это время Ассасины и Тамплиеры вели скрытую войну между собой. В отличие от Ассасинов, они думают, что человечество достигнет мира и процветания только при полном контроле всех сторон жизни общества.";
+}
+function groupchange3(){
+    document.getElementById("changeimg").src="../images/isu.png";
+    document.getElementById("changename").innerHTML="Ису";
+    document.getElementById("changetext").innerHTML="Первая Цивилизация, так же известные, как Предтечи иногда упоминаемая как 'Те, кто были до нас' ― гуманоидная раса, населявшая Землю до появления человечества.";
+}
+
+
+
+
+
+function locationchange1(){
+    document.getElementById("changeimg").src="../images/damask.jpeg";
+    document.getElementById("changename").innerHTML="Дамаск";
+    document.getElementById("changetext").innerHTML="Столица древней Сирии, Дамаск, является одним из старейших городов мира; считается, что он был основан еще в 3 тысячелетии до н.э. Дамаск расположен на восточных предгорьях Западного горного хребта Ливана, в нижнем течении реки Барада. Культурный центр Дамаск считался перекрестком всего Ближнего Востока и был концом великого Шелкового Пути. За всю свою многолетнюю историю Дамаском правили вавилонцы, персы, македонцы и римляне.";
+}
+function locationchange2(){
+    document.getElementById("changeimg").src="../images/Jerusalem.jpg";
+    document.getElementById("changename").innerHTML="Иерусалим";
+    document.getElementById("changetext").innerHTML="Расположенный на холмах Иудейских гор, Иерусалим находится в самом сердце Израиля, между Средиземным и Мертвым морем. Эго один из самых старых городов мира, переживший богатую и длинную историю.";
+}
+function locationchange3(){
+    document.getElementById("changeimg").src="../images/Acre.jpg";
+    document.getElementById("changename").innerHTML="Акра";
+    document.getElementById("changetext").innerHTML="Город Акра расположен на территории Западной Галилеи, северного региона Израиля. Огромный портовый город на севере Хайфского залива, Акра долгое время являлась стратегически важными «воротами» региона В 12 веке Крестоносцы и Сарацины постоянно сражались за город. Поэтому он, можно сказать, пережил богатую историю осад и вторжений, приводивших к постоянной смене власти.";
+}
+function locationchange4(){
+    document.getElementById("changeimg").src="../images/Florence.jpg";
+    document.getElementById("changename").innerHTML="Флоренция";
+    document.getElementById("changetext").innerHTML="Основанная еще в 80х годах до н.э., Флоренция быстро превратилась из маленького римского поселения в шумный рыночный центр. Расположенная на плодородной земле и на пути следования основных торговых маршрутов между Римом и северной Италией, Флоренция пережила сложный период после падения Римской Империи, когда за контроль над регионом боролись между собой остготы и византийцы.";
+}
+function locationchange5(){
+    document.getElementById("changeimg").src="../images/Venezia.jpg";
+    document.getElementById("changename").innerHTML="Венеция";
+    document.getElementById("changetext").innerHTML="Венеция, расположенная в Северной Италии, была возведена на островах Адриатического моря. В эпоху Ренессанса город являлся культурным, политическим и финансовым центром La Serenissima Republica di Venezia -Светлейшей Венецианской Республики. Как экономическое доминирование, так и грозный морской флот сделали Венецию площадкой для войны между Тамплиерами и Орденом Ассасинов, которые сражались за контроль над городом.";
+}
+function locationchange6(){
+    document.getElementById("changeimg").src="../images/Rome.jpg";
+    document.getElementById("changename").innerHTML="Рим";
+    document.getElementById("changetext").innerHTML="С самого своего основания, Рим стал одним из центров политического, культурного и религиозного развития Европы. С 509 года до н.э. по 476 год н.э., город являлся столицей Римской Республики и Империи. Изначально состоящий из нескольких разрозненных областей, покоящихся на Семи Холмах, Рим вскоре раскинулся за пределами реки Тибр. Рим процветал и разрастался по мере расширения границ Республики, а затем Империи.";
+}
+
+
+
+
+
+function particlechange1(){
+    document.getElementById("changeimg").src="../images/Edem-1.png";
+    document.getElementById("changename").innerHTML="Яблоко Эдема";
+    document.getElementById("changetext").innerHTML="Частица Эдема, часть древнего, но технологически передового оборудования, созданное Первой Цивилизацией для контроля над человеческим разумом.";
+}
+function particlechange2(){
+    document.getElementById("changeimg").src="../images/Edem-2.png";
+    document.getElementById("changename").innerHTML="Меч Эдема";
+    document.getElementById("changetext").innerHTML="Меч Эдема — частицы Эдема, созданные Ису. Разные Мечи были известны под разными именами. Наиболее известные - это Экскалибур и Кладенец.";
+}
+function particlechange3(){
+    document.getElementById("changeimg").src="../images/Edem-3.png";
+    document.getElementById("changename").innerHTML="Кольцо Эдема";
+    document.getElementById("changetext").innerHTML=" Частицы Эдема в виде колец. Изобретены инженерами Первой Цивилизации с целью защиты своих носителей. Кольца излучают электромагнитное поле, которое защищает носителя кольца от любых физических снарядов и лучей энергии. Кольца не дают носителю полную защиту. Кольца также используются в защитных устройствах, проецируя при использовании сферическое силовое поле синего цвета.";
+}
+function particlechange4(){
+    document.getElementById("changeimg").src="../images/Edem-4.png";
+    document.getElementById("changename").innerHTML="Хрустальный череп";
+    document.getElementById("changetext").innerHTML=" Частица Эдема Первой Цивилизации, они были разбросаны по всей Центральной и Южной Америках. В основном они были символами у ацтеков, майя, инков и других цивилизаций.";
+}
+function particlechange5(){
+    document.getElementById("changeimg").src="../images/Edem-5.png";
+    document.getElementById("changename").innerHTML="Плащаница Эдема";
+    document.getElementById("changetext").innerHTML="Частица Эдема, созданная Ису. Плащаница Эдема имеет свойства исцеления, таким образом ее владелец становится практически бессмертным.";
+}
+function particlechange6(){
+    document.getElementById("changeimg").src="../images/Edem-6.png";
+    document.getElementById("changename").innerHTML="Копьё Леонида";
+    document.getElementById("changetext").innerHTML="Копьё Леонида была артефактом Ису, принадлежащим спартанскому царю Леониду, а затем и его внуку/внучке.";
+}
+function particlechange7(){
+    document.getElementById("changeimg").src="../images/Edem-7.png";
+    document.getElementById("changename").innerHTML="Посохи Эдема";
+    document.getElementById("changetext").innerHTML="Мощные артефакты. В истории у них были разные названия, такие как ― Папский Посох или Русский императорский Скипетр. Посохи были у Египетских фараонов, русских императоров и римских пап.";
+}
